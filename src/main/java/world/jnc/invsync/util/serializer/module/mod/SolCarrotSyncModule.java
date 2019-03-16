7cd38cd6d2062aa7ebc95041c80e7cd2facc7ff8
BrainStone <yannick@tedworld.de>
2019-03-16 08:44:24 +0100

    Updated dependencies
    - Also adjust code to reduced SOL-Carrot interface. Fixes #36

--- src/main/java/world/jnc/invsync/util/serializer/module/mod/SolCarrotSyncModule.java
+++ src/main/java/world/jnc/invsync/util/serializer/module/mod/SolCarrotSyncModule.java
@@ -1,21 +1,14 @@
 package world.jnc.invsync.util.serializer.module.mod;
 
 import com.cazsius.solcarrot.capability.FoodCapability;
-import com.cazsius.solcarrot.capability.FoodInstance;
-import com.cazsius.solcarrot.handler.HandlerCapability;
+import com.cazsius.solcarrot.handler.CapabilityHandler;
 import com.cazsius.solcarrot.handler.MaxHealthHandler;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 import lombok.experimental.UtilityClass;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import org.spongepowered.api.data.DataContainer;
-import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.data.DataView;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.common.item.inventory.util.ItemStackUtil;
+import world.jnc.invsync.util.serializer.CapabilitySerializer;
 import world.jnc.invsync.util.serializer.NativeInventorySerializer;
 
 public class SolCarrotSyncModule extends BaseModSyncModule {
@@ -36,67 +29,44 @@ public class SolCarrotSyncModule extends BaseModSyncModule {
 
   @UtilityClass
   private static class Helper {
-    private static final DataQuery STACK = DataQuery.of("stack");
-    private static final DataQuery META = DataQuery.of("meta");
-
     private static DataView serialize(Player player, DataView container) {
-      FoodCapability foodCapability =
-          NativeInventorySerializer.getNativePlayer(player)
-              .getCapability(FoodCapability.FOOD_CAPABILITY, null);
-      List<DataView> serializedFoods = new LinkedList<>();
+      final EntityPlayer nativePlayer = NativeInventorySerializer.getNativePlayer(player);
 
-      if (foodCapability != null) {
-        for (FoodInstance food : foodCapability.getHistory()) {
-          DataContainer serializedFood =
-              DataContainer.createNew(DataView.SafetyMode.ALL_DATA_CLONED);
-          serializedFood.set(
-              STACK, ItemStackUtil.fromNative(new net.minecraft.item.ItemStack(food.item())));
-          serializedFood.set(META, food.meta());
-          serializedFoods.add(serializedFood);
-        }
-      }
-
-      container.set(THIS, serializedFoods);
+      container.set(
+          THIS,
+          CapabilitySerializer.serializeCapabilityToView(
+              FoodCapability.FOOD_CAPABILITY, nativePlayer));
 
       return container;
     }
 
     private static void deserialize(Player player, DataView container) {
-      Optional<List<DataView>> serializedFoods = container.getViewList(THIS);
+      Optional<DataView> serializedFoods = container.getView(THIS);
+      int foodCount = 0;
 
       if (serializedFoods.isPresent()) {
-        // Deserialize to a new instance first. This way if anything goes wrong, an exception is
-        // raised before
-        // the real food list is cleared.
-        FoodCapability foodCapability = new FoodCapability();
-        for (DataView serializedFood : serializedFoods.get()) {
-          ItemStack food_stack =
-              ItemStack.builder().fromContainer(serializedFood.getView(STACK).get()).build();
-          int meta = serializedFood.getInt(META).get();
-          Item food_item = (Item) food_stack.getType();
+        final EntityPlayer nativePlayer = NativeInventorySerializer.getNativePlayer(player);
 
-          foodCapability.addFood(food_item, meta);
-        }
+        CapabilitySerializer.deserializeCapabilityFromView(
+            FoodCapability.FOOD_CAPABILITY, nativePlayer, serializedFoods.get());
 
-        if (getDebug()) {
-          getLogger().info("\t\tisPresent:");
-          getLogger().info("\t\t\tserializedFoods:\t" + serializedFoods.isPresent());
-        }
-
-        // Now copy the food list to the player
-        EntityPlayer nativePlayer = NativeInventorySerializer.getNativePlayer(player);
-        FoodCapability playerFoodCapability =
+        final FoodCapability foodCapability =
             nativePlayer.getCapability(FoodCapability.FOOD_CAPABILITY, null);
 
-        if (playerFoodCapability != null) {
-          playerFoodCapability.copyFoods(foodCapability);
-          HandlerCapability.syncFoodList(nativePlayer);
+        if (foodCapability != null) {
+          CapabilityHandler.syncFoodList(nativePlayer);
 
           // Now that the food list has been synchronized, use it to set the player's max health.
           MaxHealthHandler.updateFoodHPModifier(nativePlayer);
 
-          if (getDebug()) getLogger().info("\t\t\tfoodCount:\t" + foodCapability.getCount());
+          foodCount = foodCapability.getFoodCount();
         }
+      }
+
+      if (getDebug()) {
+        getLogger().info("\t\tisPresent:");
+        getLogger().info("\t\t\tserializedFoods:\t" + serializedFoods.isPresent());
+        if (serializedFoods.isPresent()) getLogger().info("\t\t\tfoodCount:\t" + foodCount);
       }
     }
   }
